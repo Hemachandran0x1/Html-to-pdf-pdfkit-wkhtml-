7f0855487814109e6b1e6b38bb23d569abb3440d
Hemachandran0x1 <phemachandran173@gmail.com>
2021-11-04 21:10:40 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pdfkit
+import argparse
 
-x = input('Enter Url:')
-y = input('Enter pdf name:') + '.pdf'
-pdfkit.from_url(x, y)
+parser = argparse.ArgumentParser(description="Html to Pdf")
+parser.add_argument("-u", "--url", help="To convert online resource into pdf ", default=0)
+parser.add_argument("-f", "--file", help="To convert downloaded html to pdf", default=0)
+args = parser.parse_args()
+if args.url:
+    x = input('Enter pdf name:') + '.pdf'
+    pdfkit.from_url(args.url, x)
+elif args.file:
+    y = input('Enter pdf name:')+'.pdf'
+    pdfkit.from_file(args.file, y)
+else:
+    print("use \"-h\" as command line argument for help")
+
